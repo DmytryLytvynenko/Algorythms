e36bd96e7e0e18b4c4ef90823c3b6c1e7887e39a
DmytryLytvynenko <dimdimalit@gmail.com>
2023-11-12 16:25:42 +0200

    Turing Machine Update

--- Алгоритми Серпінська/Dikstra/Dikstra/Program.cs
+++ Алгоритми Серпінська/Dikstra/Dikstra/Program.cs
@@ -1,87 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Dikstra
+namespace DikstraAlgorithm
 {
-    class Program
-    {
-        public static string FindPathLength(string path, Graph g)
-        {
-            int length = 0;
-            if (path.Length < 1)
-            {
-                return "0";
-            }
-            else
-            {
-                for (int i = 0; i < path.Length - 1; i++)
-                {
-                    length += g.FindEdge(path[i].ToString(), path[i + 1].ToString()).EdgeWeight;
-                }
-            }
-            return length.ToString();
-        }
-        private static string FindBestVertex(Graph g)
-        {
-            var dijkstra = new Dijkstra(g);
-            string bestVertex = "";
-            int min = 100000;
-            int globalLength = 0;
-            foreach (var v in g.Vertices)
-            {
-                foreach (var k in g.Vertices)
-                {
-                    var path = dijkstra.FindShortestPath(v.Name, k.Name);
-                    globalLength += Convert.ToInt32(FindPathLength(path.ToString(), g));
-                }
-                if (globalLength < min)
-                {
-                    min = globalLength;
-                    bestVertex = v.Name;
-                }
-                Console.WriteLine(v.Name + " " + globalLength.ToString() + "\n");
-                globalLength = 0;
-            }
-            return "\nВiдповiдь: " + bestVertex;
-        }
-        static void Main(string[] args)
-        {
-            var g = new Graph();
+	class Dijkstra
+	{
+		public static int V = 9;
+		int minDistance(int[] dist,
+						bool[] sptSet)
+		{
+			int min = int.MaxValue, min_index = -1;
 
-            //додавання вершин
-            g.AddVertex("1");
-            g.AddVertex("2");
-            g.AddVertex("3");
-            g.AddVertex("4");
-            g.AddVertex("5");
-            g.AddVertex("6");
-            g.AddVertex("7");
-            g.AddVertex("8");
-            g.AddVertex("9");
+			for (int v = 0; v < V; v++)
+				if (sptSet[v] == false && dist[v] <= min)
+				{
+					min = dist[v];
+					min_index = v;
+				}
 
-            //додавання ребер
-            g.AddEdge("1", "2", 7);
-            g.AddEdge("1", "4", 4);
-            g.AddEdge("1", "3", 10);
-            g.AddEdge("2", "6", 12);
-            g.AddEdge("2", "4", 5);
-            g.AddEdge("3", "5", 6);
-            g.AddEdge("3", "8", 5);
-            g.AddEdge("4", "6", 6);
-            g.AddEdge("4", "5", 3);
-            g.AddEdge("4", "7", 7);
-            g.AddEdge("5", "8", 8);
-            g.AddEdge("6", "9", 9);
-            g.AddEdge("7", "9", 8);
-            g.AddEdge("7", "8", 10);
-            g.AddEdge("8", "9", 11);
-            
-            Console.WriteLine(FindBestVertex(g));
-            Console.ReadLine();
-        }
+			return min_index;
+		}
 
-    }
+		void printSolution(int[] dist, int n)
+		{
+			Console.Write("Vertex	 Distance "
+						+ "from Source\n");
+			for (int i = 0; i < V; i++)
+				Console.Write(i+1 + " \t\t " + dist[i] + "\n");
+		}
+		void dijkstra(int[,] graph, int src)
+		{
+			int[] dist = new int[V]; 
+
+			bool[] sptSet = new bool[V];
+
+			for (int i = 0; i < V; i++)
+			{
+				dist[i] = int.MaxValue;
+				sptSet[i] = false;
+			}
+			dist[src] = 0;
+
+			for (int count = 0; count < V - 1; count++)
+			{
+				int u = minDistance(dist, sptSet);
+
+				sptSet[u] = true;
+
+				for (int v = 0; v < V; v++)
+					if (!sptSet[v] && graph[u, v] != 0 &&
+						dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+						dist[v] = dist[u] + graph[u, v];
+			}
+
+			printSolution(dist, V);
+		}
+
+		public static void Main()
+		{
+			int source = 0;
+			int[,] graph = new int[,] { { 0, 1, 5, 8, 0, 0, 0, 0},
+									    { 1, 0, 0, 3, 2, 0, 0, 0},
+									    { 5, 0, 0, 2, 0, 4, 0, 0},
+									    { 8, 3, 2, 0, 6, 7, 0, 0},
+									    { 0, 2, 0, 6, 0, 1, 6, 0},
+									    { 0, 0, 4, 7, 1, 0, 3, 3},
+									    { 0, 0, 0, 0, 6, 3, 0, 8},
+									    { 0, 0, 0, 0, 0, 3, 8, 0} };
+			Dijkstra.V = 8;
+			Dijkstra t = new Dijkstra();
+            Console.WriteLine("Source: " + (source + 1) + " vertex");
+            Console.WriteLine();
+			t.dijkstra(graph, source);
+
+			Console.ReadLine();
+		}
+	}
 }
--- Алгоритми Серпінська/TuringMachine/TuringMachine/Program.cs
+++ Алгоритми Серпінська/TuringMachine/TuringMachine/Program.cs
@@ -8,25 +8,41 @@ namespace TuringMachine
 {
     class Program
     {
+        public enum MoveAction 
+        {
+            Stay,
+            Right,
+            Left
+        }
         static void Main(string[] args)
         {
             string startString = "abbaabba";
             int startHeadPosition = 2;
-            char[] alphavite = new char[] { 'a', 'b' };
+            List<char> alphavite = new List<char> {' ', 'a', 'b' };
             bool moovingDirectionRight = false;
-            bool headReachedEnd = false;
-            TuringMachine turingMachine = new TuringMachine(startString, startHeadPosition, moovingDirectionRight);
+            TuringMachine turingMachine = new TuringMachine(startString, startHeadPosition, moovingDirectionRight, alphavite);
+            State q1 = new State(turingMachine);
+            State q2 = new State(turingMachine);
+            State q3 = new State(turingMachine);
+            State q4 = new State(turingMachine);
+            turingMachine.InitializeStartState(q1);
 
+            q1.AddCommand('a', TuringMachine.MoveAction.Left, false, q1);
+            q1.AddCommand('b', TuringMachine.MoveAction.Left, false, q1);
+            q1.AddCommand(' ', TuringMachine.MoveAction.Stay, false, q2);
+
+            q2.AddCommand('a', TuringMachine.MoveAction.Right, false, q3);
+            q2.AddCommand('b', TuringMachine.MoveAction.Right, false, q3);
+            q2.AddCommand(' ', TuringMachine.MoveAction.Right, false, q3);
+
+            q3.AddCommand('a', TuringMachine.MoveAction.Right, false, q4);
+            q3.AddCommand('a', TuringMachine.MoveAction.Right, false, q4);
+
+            q4.AddCommand('a', TuringMachine.MoveAction.Stay, true, null,'1');
+            q4.AddCommand('b', TuringMachine.MoveAction.Stay, true, null,'1');
             turingMachine.ShowBody();
 
-            while (!headReachedEnd)
-            {
-                headReachedEnd = turingMachine.MoveHead();
-                turingMachine.ShowBody();
-            }
-            turingMachine.ChangeMoveDirection();
-            turingMachine.MoveHead();
-            turingMachine.PasteChar('1');
+            turingMachine.StartProdram();
 
             turingMachine.ShowBody();
             Console.ReadKey();
@@ -35,74 +51,83 @@ namespace TuringMachine
 
     public class TuringMachine
     {
+        public enum MoveAction
+        {
+            Stay,
+            Right,
+            Left
+        }
         public bool MoveRight { get; private set; } = true;
 
         private int head = 0;
         private List<char> body = new List<char>();
-        public TuringMachine(string startString, int headPosition, bool moovingDirectionRight)
+        private List<char> alphavite = new List<char>();
+        private State startState;
+        public TuringMachine(string startString, int headPosition, bool moovingDirectionRight, List<char> _alphavite)
         {
             body.AddRange(startString);
             head = headPosition;
             MoveRight = moovingDirectionRight;
+            alphavite = _alphavite;
         }
-
-        public string GetBody()
+        public void InitializeStartState(State _startState)
         {
-            return new string(body.ToArray());
+            startState = _startState;
+        }
+        public char GetAlphaviteByIndex(int index)
+        {
+            return alphavite[index];
+        }
+        public void MoveHead(MoveAction action)
+        {
+            switch (action)
+            {
+                case MoveAction.Stay:
+                    break;
+                case MoveAction.Right:
+                    head++;
+                    if (head == body.Count)
+                    {
+                        body.Add(alphavite[0]);
+                    }
+                    break;
+                case MoveAction.Left:
+                    head--;
+                    if (head == -1)
+                    {
+                        PasteFirst(alphavite[0]);
+                        head = 0;
+                    }
+                    break;
+                default:
+                    break;
+            }
         }        
-        public void ShowBody()
+        public void StartProdram()
         {
-            string body = GetBody();
-            for (int i = 0; i < head; i++)
-            {
-                Console.Write(body[i]);
+            bool endOfProgramm = false;
+            int returnCode;
+            State currentState = startState;
+
+            while (!endOfProgramm)
+            { 
+                if (currentState == null)
+                {
+                    Console.WriteLine("currentState is null");
+                    return;
+                }
+                returnCode = currentState.ExecuteState(GetCellValue());
+                if (returnCode == 1)
+                {
+                    endOfProgramm = true;
+                    Console.WriteLine("End of program!");
+                }
+                else
+                {
+                    currentState = currentState.NextState;
+                }
             }
 
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write(body[head]);
-            Console.ResetColor();
-
-            for (int i = head + 1; i < body.Length; i++)
-            {
-                Console.Write(body[i]);
-            }
-
-            Console.WriteLine();
-        }
-        private char GetCellValue() 
-        {
-            return body[head];
-        }
-        public bool MoveHead()
-        {
-            bool headReachedEnd;
-            if (MoveRight)
-            {
-                head++;
-                headReachedEnd = head >= body.Count - 1;
-                if (headReachedEnd)
-                    head = body.Count - 1;
-
-                return headReachedEnd;
-            }
-            else
-            {
-                head--;
-                headReachedEnd = head <= 0;
-                if (headReachedEnd)
-                    head = 0;
-
-                return headReachedEnd;
-            }
-
-            if (headReachedEnd)
-                return headReachedEnd;
-
-
-        }
-        public void ChangeMoveDirection()
-        {
-            MoveRight = !MoveRight;
         }
         public bool CompareCell(char value)
         {
@@ -132,9 +157,110 @@ namespace TuringMachine
             }
             body[head] = value;
         }
+        public void PasteFirst(char value)
+        {
+            body.Add(' ');
+
+            char temp;
+
+            for (int i = body.Count - 1; i > 0; i--)
+            {
+                temp = body[i - 1];
+                body[i] = temp;
+            }
+            body[0] = value;
+        }
         public void PasteCharLast(char value)
         {
             body.Add(value);
+        }
+        private string GetBody()
+        {
+            return new string(body.ToArray());
+        }
+        public void ShowBody()
+        {
+            string body = GetBody();
+            for (int i = 0; i < head; i++)
+            {
+                Console.Write(body[i]);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(body[head]);
+            Console.ResetColor();
+
+            for (int i = head + 1; i < body.Length; i++)
+            {
+                Console.Write(body[i]);
+            }
+
+            Console.WriteLine();
+        }
+        public char GetCellValue() 
+        {
+            return body[head];
+        }
+    }
+    public class State
+    {
+        public List<Command> commands = new List<Command>();
+        public TuringMachine TuringMachine { get; private set; }
+        public State NextState { get; private set; }
+        public State(TuringMachine turingMachine)
+        {
+            TuringMachine = turingMachine;
+        }
+        public void AddCommand(char symbol, TuringMachine.MoveAction moveAction = TuringMachine.MoveAction.Stay, bool endOfStates = false, State nextState = null, char writeSymbol = '_')
+        {
+            commands.Add(new Command(symbol, moveAction, endOfStates, nextState, writeSymbol));
+        }
+        public int ExecuteState(char input)
+        {
+            int returnCode = 1;
+            for (int i = 0; i < commands.Count; i++)
+            {
+                if (input == commands[i].Symbol)
+                {
+                    NextState = commands[i].NextState;
+                    returnCode = commands[i].ExecuteCommand(TuringMachine);
+                    break;
+                }
+            }
+            return returnCode;
+        }
+        public class Command
+        {
+            public char Symbol { get; private set; }
+            public bool EndOfStates { get; private set; }
+            public State NextState { get; private set; }
+            public char WriteSymbol { get; private set; }
+            public TuringMachine.MoveAction MoveAction { get; private set; }
+
+            public Command(char symbol, TuringMachine.MoveAction moveAction = TuringMachine.MoveAction.Stay, bool endOfStates = false, State nextState = null, char writeSymbol = '_')
+            {
+                Symbol = symbol;
+                WriteSymbol = writeSymbol;
+                MoveAction = moveAction;
+                NextState = nextState;
+                EndOfStates = endOfStates;
+            }
+            public int ExecuteCommand(TuringMachine turingMachine)
+            {
+
+                if (WriteSymbol != '_')
+                {
+                    turingMachine.PasteChar(WriteSymbol);
+                }
+                turingMachine.MoveHead(MoveAction);
+
+
+                if (EndOfStates)
+                {
+                    return 1;
+                }
+                return 0;
+            }
         }
     }
 }
